f0aed3355416a165eb66b73b8e1fb48bb97862e2
Thoriq R <respect.thoriq@gmail.com>
2023-04-10 11:14:01 +0700

    Making province and city data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('/home');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/api/province/{id}/cities', [HomeController::class, 'getCities']);
